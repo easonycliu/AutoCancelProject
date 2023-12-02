1136b567f1492cf6d3d7118fcd399a655d5c5b15
liuyc1515 <liuyicheng1515@sjtu.edu.cn>
2023-12-01 19:41:54 -0700

    fix problem in solr

--- scripts/microbenchmark/abnormal_sensitivity/abnormal_sensitivity.sh
+++ scripts/microbenchmark/abnormal_sensitivity/abnormal_sensitivity.sh
@@ -10,11 +10,11 @@ export START_DATE=$(date +%Y_%m_%d)
 
 declare -A case_to_script_map
 case_to_script_map["c1"]="elasticsearch_exp multiclient_request_cache_evict"
-case_to_script_map["c2"]="elasticsearch_exp multiclient_update_by_query"
+# case_to_script_map["c2"]="elasticsearch_exp multiclient_update_by_query"
 case_to_script_map["c3"]="elasticsearch_exp multiclient_nested_aggs"
 case_to_script_map["c4"]="elasticsearch_exp multiclient_complex_boolean"
 case_to_script_map["c5"]="elasticsearch_exp multiclient_bulk_large_document"
-case_to_script_map["c6"]="solr_exp complex_boolean_script"
+# case_to_script_map["c6"]="solr_exp complex_boolean_script"
 case_to_script_map["c7"]="solr_exp stat_fields"
 
 client_num=5
@@ -36,9 +36,9 @@ function run_once {
     local app_exp=$(echo ${case_to_script_map["$6"]} | awk '{print $1}')
     local case_name=$(echo ${case_to_script_map["$6"]} | awk '{print $2}')
 
-    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/docker_config.yml down
+    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down
 
-    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/docker_config.yml up &
+    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml up &
     sleep 60
 
     docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/$app_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/warmup.sh
@@ -54,7 +54,7 @@ function run_once {
     sudo mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_latency.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}_latency.csv
     sudo mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_throughput.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}_throughput.csv
 
-    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/docker_config.yml down
+    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down
 }
 
 function run_case {
@@ -94,11 +94,11 @@ function run_round {
 # sleep 10
 # run_round c3
 # sleep 10
-run_round c4
-sleep 10
-# run_round c5
+# run_round c4
 # sleep 10
+run_round c5
+sleep 10
 # run_round c6
 # sleep 10
-# run_round c7
-# sleep 10
+run_round c7
+sleep 10
