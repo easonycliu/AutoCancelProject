d650987afd3e491cf149b14bcc334de0dc198102
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-03-20 14:34:12 +0800

    remove solr bench source code from scripts

--- scripts/microbenchmark/solr-bench/src/main/java/org/apache/solr/benchmarks/query/QueryResponseContentsListener.java
+++
@@ -1,7 +0,0 @@
-package org.apache.solr.benchmarks.query;
-
-import org.apache.solr.benchmarks.BenchmarksMain;
-import org.apache.solr.benchmarks.ControlledExecutor;
-
-public interface QueryResponseContentsListener extends ControlledExecutor.ExecutionListener<BenchmarksMain.OperationKey, QueryResponseContents> {
-}
--- scripts/microbenchmark/solr-bench/src/main/java/org/apache/solr/benchmarks/readers/Sink.java
+++
@@ -1,7 +0,0 @@
-package org.apache.solr.benchmarks.readers;
-
-public interface Sink<T> {
-
-    boolean item(T doc);
-}
-
