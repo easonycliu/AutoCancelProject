d58b328f858975ef7296b1417f5af156863b2c30
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-03-26 12:52:00 +0800

    change cpu and heap from script

--- scripts/microbenchmark/abnormal_sensitivity/abnormal_sensitivity.sh
+++ scripts/microbenchmark/abnormal_sensitivity/abnormal_sensitivity.sh
@@ -9,13 +9,13 @@ export START_TIME=$(date +%Y_%m_%d_%H_%M_%S)
 export START_DATE=$(date +%Y_%m_%d)
 
 declare -A case_to_script_map
-case_to_script_map["c1"]="elasticsearch_exp multiclient_request_cache_evict"
-# case_to_script_map["c2"]="elasticsearch_exp multiclient_update_by_query"
-case_to_script_map["c3"]="elasticsearch_exp multiclient_nested_aggs"
-case_to_script_map["c4"]="elasticsearch_exp multiclient_complex_boolean"
-case_to_script_map["c5"]="elasticsearch_exp multiclient_bulk_large_document"
-# case_to_script_map["c6"]="solr_exp complex_boolean_script"
-case_to_script_map["c7"]="solr_exp stat_fields"
+case_to_script_map["c1"]="elasticsearch_exp multiclient_request_cache_evict 8.00 16"
+# case_to_script_map["c2"]="elasticsearch_exp multiclient_update_by_query 8.00 16"
+case_to_script_map["c3"]="elasticsearch_exp multiclient_nested_aggs 8.00 8"
+case_to_script_map["c4"]="elasticsearch_exp multiclient_complex_boolean 8.00 16"
+case_to_script_map["c5"]="elasticsearch_exp multiclient_bulk_large_document 8.00 16"
+# case_to_script_map["c6"]="solr_exp complex_boolean_script 2.00 16"
+case_to_script_map["c7"]="solr_exp stat_fields 4.00 16"
 
 client_num=5
 
@@ -35,10 +35,14 @@ fi
 function run_once {
     local app_exp=$(echo ${case_to_script_map["$6"]} | awk '{print $1}')
     local case_name=$(echo ${case_to_script_map["$6"]} | awk '{print $2}')
+    local core_num=$(echo ${case_to_script_map["$6"]} | awk '{print $3}')
+    local heap_size=$(echo ${case_to_script_map["$6"]} | awk '{print $4}')
 
-    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down
+	local env_args="DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 CORE_NUM=$core_num HEAP_SIZE=$heap_size"
 
-    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml up &
+	bash -c "$env_args docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down"
+
+	bash -c "$env_args docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml up &"
     sleep 60
 
     docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/$app_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/warmup.sh
@@ -54,7 +58,7 @@ function run_once {
     sudo mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_latency.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}_latency.csv
     sudo mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_throughput.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}_throughput.csv
 
-    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down
+	bash -c "$env_args docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down"
 }
 
 function run_case {
@@ -78,27 +82,27 @@ function run_case {
 }
 
 function run_round {
-    # run_case $1 0.1 40
-    # sleep 10
-    # run_case $1 0.25 100
-    # sleep 10
-    run_case $1 0.5 200
+    run_case $1 0.1 80
     sleep 10
-    run_case $1 0.75 300
+    run_case $1 0.25 200
+    sleep 10
+    run_case $1 0.5 400
+    sleep 10
+    run_case $1 0.75 600
     sleep 10
 }
 
-# run_round c1
-# sleep 10
+run_round c1
+sleep 10
 # run_round c2
 # sleep 10
 # run_round c3
 # sleep 10
 # run_round c4
 # sleep 10
-run_round c5
-sleep 10
+# run_round c5
+# sleep 10
 # run_round c6
 # sleep 10
-run_round c7
-sleep 10
+# run_round c7
+# sleep 10
