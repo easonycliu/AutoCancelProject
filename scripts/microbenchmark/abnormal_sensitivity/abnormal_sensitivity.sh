9245ef82ca331fb9ea1daceff609f77ba9844617
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-03-26 14:10:56 +0800

    finish interval_sensitivity and run one case every time

--- scripts/microbenchmark/abnormal_sensitivity/abnormal_sensitivity.sh
+++ scripts/microbenchmark/abnormal_sensitivity/abnormal_sensitivity.sh
@@ -95,17 +95,7 @@ function run_round {
     sleep 10
 }
 
-run_round c1
-sleep 10
-# run_round c2
-# sleep 10
-# run_round c3
-# sleep 10
-# run_round c4
-# sleep 10
-# run_round c5
-# sleep 10
-# run_round c6
-# sleep 10
-# run_round c7
-# sleep 10
+if [[ "$1" =~ ^c[1-7]$ ]]; then
+	run_round $1
+fi
+
--- scripts/microbenchmark/interval_sensitivity/interval_sensitivity.sh
+++ scripts/microbenchmark/interval_sensitivity/interval_sensitivity.sh
@@ -8,45 +8,92 @@ export MICROBENCHMARK=interval_sensitivity
 export START_TIME=$(date +%Y_%m_%d_%H_%M_%S)
 export START_DATE=$(date +%Y_%m_%d)
 
+declare -A case_to_script_map
+case_to_script_map["c1"]="elasticsearch_exp multiclient_request_cache_evict 8.00 16"
+# case_to_script_map["c2"]="elasticsearch_exp multiclient_update_by_query 8.00 16"
+case_to_script_map["c3"]="elasticsearch_exp multiclient_nested_aggs 8.00 8"
+case_to_script_map["c4"]="elasticsearch_exp multiclient_complex_boolean 8.00 16"
+case_to_script_map["c5"]="elasticsearch_exp multiclient_bulk_large_document 8.00 16"
+# case_to_script_map["c6"]="solr_exp complex_boolean_script 2.00 16"
+case_to_script_map["c7"]="solr_exp stat_fields 4.00 16"
+
 client_num=5
 
 sudo sysctl -w vm.max_map_count=262144
 
-if [ ! -d "$AUTOCANCEL_HOME/scripts/logs/$START_DATE" ]; then
-    sudo mkdir $AUTOCANCEL_HOME/scripts/logs/$START_DATE
+if [ $(docker images | grep "solr_exp" | wc -l) -eq 0 ]; then
+    docker build --build-arg SOLR_ID=$(id -u) -t solr_exp:v1.0-9.0.0 .
 fi
 
-sudo mkdir $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}
-sudo chown -R 1000:1000 $AUTOCANCEL_HOME/scripts/logs
+if [ ! -d "$AUTOCANCEL_HOME/scripts/logs/$START_DATE" ]; then
+    mkdir $AUTOCANCEL_HOME/scripts/logs/$START_DATE
+fi
+
+mkdir $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}
 
 if [ ! -f "$AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp/query/boolean_search.json" ]; then
     docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/performance_issues/complex_boolean_operations.py 2000 boolean_search.json
 fi
 
 function run_once {
-    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 UPDATE_INTERVAL=$5 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/interval_sensitivity/docker_config.yml down
+    local app_exp=$(echo ${case_to_script_map["$6"]} | awk '{print $1}')
+    local case_name=$(echo ${case_to_script_map["$6"]} | awk '{print $2}')
+    local core_num=$(echo ${case_to_script_map["$6"]} | awk '{print $3}')
+    local heap_size=$(echo ${case_to_script_map["$6"]} | awk '{print $4}')
 
-    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 UPDATE_INTERVAL=$5 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/interval_sensitivity/docker_config.yml up &
+	local env_args="USER_ID=$(id -u) GROUP_ID=$(id -g) DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 UPDATE_INTERVAL=$7 CORE_NUM=$core_num HEAP_SIZE=$heap_size"
+
+	bash -c "$env_args docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down"
+
+	bash -c "$env_args docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml up &"
     sleep 60
 
-    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/warmup.sh
+    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/$app_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/warmup.sh
     sleep 10
 
-    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/multiclient_complex_boolean.sh $6 ${4}_${START_TIME} $4
+    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/$app_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/$case_name.sh $5 ${4}_${START_TIME} $4
     sleep 10
 
-    sudo mv $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp/${4}_${START_TIME}_latency $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${CASE}_${START_TIME}/${4}_latency.csv
-    sudo mv $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp/${4}_${START_TIME}_throughput $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${CASE}_${START_TIME}/${4}_throughput.csv
+    sudo mv $AUTOCANCEL_HOME/autocancel_exp/$app_exp/${4}_${START_TIME}_latency $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_latency.csv
+    sudo mv $AUTOCANCEL_HOME/autocancel_exp/$app_exp/${4}_${START_TIME}_throughput $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_throughput.csv
 
-    sudo mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_${5}_${6}.csv
-    sudo mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_latency.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_${5}_${6}_latency.csv
+    sudo mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}.csv
+    sudo mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_latency.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}_latency.csv
+    sudo mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_throughput.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}_throughput.csv
 
-    DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 UPDATE_INTERVAL=$5 docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/interval_sensitivity/docker_config.yml down
+	bash -c "$env_args docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down"
 }
 
-run_once multi_objective_policy true false normal 80 $client_num
-sleep 10
-run_once multi_objective_policy true false normal 100 $client_num
-sleep 10
-run_once multi_objective_policy true false normal 120 $client_num
-sleep 10
+function run_case {
+    run_once base_policy false true base_wo_predict $client_num $1 $2
+    sleep 10
+
+    run_once base_policy true true base_w_predict $client_num $1 $2
+    sleep 10
+
+    run_once multi_objective_policy false true moo_wo_predict $client_num $1 $2
+    sleep 10
+
+    run_once multi_objective_policy true true moo_w_predict $client_num $1 $2
+    sleep 10
+
+    run_once multi_objective_policy true false wo_cancel $client_num $1 $2
+    sleep 10
+
+    run_once multi_objective_policy true false normal $client_num $1 $2
+    sleep 10
+}
+
+function run_round {
+    run_case $1 50
+    sleep 10
+    run_case $1 100
+    sleep 10
+    run_case $1 200
+    sleep 10
+}
+
+if [[ "$1" =~ ^c[1-7]$ ]]; then
+	run_round $1
+fi
+
