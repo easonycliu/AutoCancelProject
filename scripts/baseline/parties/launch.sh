e1e4afd165f5a9c9999859e223adb697671d157d
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-03-28 16:33:25 +0800

    add launch script for parties

--- scripts/baseline/parties/launch.sh
+++ scripts/baseline/parties/launch.sh
@@ -10,6 +10,9 @@ export START_DATE=$(date +%Y_%m_%d)
 
 client_num=2
 
+cgroup_num=$(cat config.txt | head -1)
+cgroup_names=($(cat config.txt | tail -n $cgroup_num | awk '{print $1}'))
+
 sudo sysctl -w vm.max_map_count=262144
 
 if [ $(docker images | grep "solr_exp" | wc -l) -eq 0 ]; then
@@ -38,14 +41,33 @@ docker rm single_node || true
 bash -c "$env_args docker compose -f $AUTOCANCEL_HOME/scripts/cases/c6_complex_request/docker_config.yml up &"
 sleep 60
 
+docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/warmup.sh
+sleep 10
+
 # Set up cgroup for PARTIES
+for cgroup_name in ${cgroup_names[@]}; do
+	sudo cgdelete -g cpuset:/$cgroup_name
+	sudo cgcreate -g cpuset:/$cgroup_name
+	echo "0" | sudo tee /sys/fs/cgroup/cpuset/$cgroup_name/cpuset.mems
+	echo "0-$(( `proc` - 1 ))" | sudo tee /sys/fs/cgroup/cpuset/$cgroup_name/cpuset.cpus
+done
+
+target_pid=$(ps -aux | grep "autocancel.log" | head -1 | awk '{print $2}')
+target_threads=($(ps -o spid= -T $target_pid))
+target_threads_num=$(echo ${target_threads[@]} | wc -w)
+for index in $(seq 0 1 $(( target_threads_num - 1 ))); do
+	echo ${target_threads[$index]} | sudo tee /sys/fs/cgroup/cpuset/${cgroup_names[$(( index % cgroup_num ))]}/tasks > /dev/null
+done
 # Finish set up cgroup for PARTIES
 
 # Launch PARTIES
+./parties.py $AUTOCANCEL_HOME/scripts/baseline/parties/config.txt 10000
 # Finish launch PARTIES
 
 docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/solr_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/complex_boolean_script.sh $client_num ${BASELINE}_${START_TIME} $BASELINE
 sleep 10
+
+kill -2 $(ps | grep parties.py | awk '{print $1}')
 
 mv $AUTOCANCEL_HOME/autocancel_exp/solr_exp/${BASELINE}_${START_TIME}_latency $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${BASELINE}_${START_TIME}/${BASELINE}_latency.csv
 mv $AUTOCANCEL_HOME/autocancel_exp/solr_exp/${BASELINE}_${START_TIME}_throughput $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${BASELINE}_${START_TIME}/${BASELINE}_throughput.csv
