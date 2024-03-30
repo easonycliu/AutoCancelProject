d2a775417f6b72aaf871f0416f3434019762fd35
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-03-30 14:09:06 +0800

    change abnormal and interval exp client num to case wise

--- scripts/microbenchmark/abnormal_sensitivity/abnormal_sensitivity.sh
+++ scripts/microbenchmark/abnormal_sensitivity/abnormal_sensitivity.sh
@@ -9,15 +9,13 @@ export START_TIME=$(date +%Y_%m_%d_%H_%M_%S)
 export START_DATE=$(date +%Y_%m_%d)
 
 declare -A case_to_script_map
-case_to_script_map["c1"]="elasticsearch_exp multiclient_request_cache_evict 8.00 16"
-# case_to_script_map["c2"]="elasticsearch_exp multiclient_update_by_query 8.00 16"
-case_to_script_map["c3"]="elasticsearch_exp multiclient_nested_aggs 8.00 8"
-case_to_script_map["c4"]="elasticsearch_exp multiclient_complex_boolean 8.00 16"
-case_to_script_map["c5"]="elasticsearch_exp multiclient_bulk_large_document 8.00 16"
-case_to_script_map["c6"]="solr_exp complex_boolean_script 2.00 16"
-case_to_script_map["c7"]="solr_exp stat_fields 4.00 16"
-
-client_num=5
+case_to_script_map["c1"]="elasticsearch_exp multiclient_request_cache_evict 8.00 16 5"
+# case_to_script_map["c2"]="elasticsearch_exp multiclient_update_by_query 8.00 16 8"
+case_to_script_map["c3"]="elasticsearch_exp multiclient_nested_aggs 8.00 8 8"
+case_to_script_map["c4"]="elasticsearch_exp multiclient_complex_boolean 8.00 16 5"
+case_to_script_map["c5"]="elasticsearch_exp multiclient_bulk_large_document 8.00 16 5"
+case_to_script_map["c6"]="solr_exp complex_boolean_script 2.00 16 2"
+case_to_script_map["c7"]="solr_exp stat_fields 4.00 16 8"
 
 sudo sysctl -w vm.max_map_count=262144
 
@@ -36,12 +34,13 @@ if [ ! -f "$AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp/query/boolean_searc
 fi
 
 function run_once {
-    local app_exp=$(echo ${case_to_script_map["$6"]} | awk '{print $1}')
-    local case_name=$(echo ${case_to_script_map["$6"]} | awk '{print $2}')
-    local core_num=$(echo ${case_to_script_map["$6"]} | awk '{print $3}')
-    local heap_size=$(echo ${case_to_script_map["$6"]} | awk '{print $4}')
+    local app_exp=$(echo ${case_to_script_map["$5"]} | awk '{print $1}')
+    local case_name=$(echo ${case_to_script_map["$5"]} | awk '{print $2}')
+    local core_num=$(echo ${case_to_script_map["$5"]} | awk '{print $3}')
+    local heap_size=$(echo ${case_to_script_map["$5"]} | awk '{print $4}')
+    local client_num=$(echo ${case_to_script_map["$5"]} | awk '{print $5}')
 
-	local env_args="USER_ID=$(id -u) GROUP_ID=$(id -g) DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$7 ABNORMAL_ABSOLUTE=$8 CORE_NUM=$core_num HEAP_SIZE=$heap_size"
+	local env_args="USER_ID=$(id -u) GROUP_ID=$(id -g) DEFAULT_POLICY=$1 PREDICT_PROGRESS=$2 CANCEL_ENABLE=$3 AUTOCANCEL_LOG=$4 ABNORMAL_PORTION=$6 ABNORMAL_ABSOLUTE=$7 CORE_NUM=$core_num HEAP_SIZE=$heap_size"
 
 	bash -c "$env_args docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down"
 
@@ -51,36 +50,36 @@ function run_once {
     docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/$app_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/warmup.sh
     sleep 10
 
-    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/$app_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/$case_name.sh $5 ${4}_${START_TIME} $4
+    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/$app_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/scripts/$case_name.sh $client_num ${4}_${START_TIME} $4
     sleep 10
 
     mv $AUTOCANCEL_HOME/autocancel_exp/$app_exp/${4}_${START_TIME}_latency $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_latency.csv
     mv $AUTOCANCEL_HOME/autocancel_exp/$app_exp/${4}_${START_TIME}_throughput $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_throughput.csv
 
-    mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}.csv
-    mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_latency.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}_latency.csv
-    mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_throughput.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${6}_${4}_${7}_${8}_throughput.csv
+    mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${5}_${4}_${6}_${7}.csv
+    mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_latency.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${5}_${4}_${6}_${7}_latency.csv
+    mv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${4}_throughput.csv $AUTOCANCEL_HOME/scripts/logs/$START_DATE/${MICROBENCHMARK}_${START_TIME}/${5}_${4}_${6}_${7}_throughput.csv
 
 	bash -c "$env_args docker compose -f $AUTOCANCEL_HOME/scripts/microbenchmark/abnormal_sensitivity/${app_exp}_docker_config.yml down"
 }
 
 function run_case {
-    run_once base_policy false true base_wo_predict $client_num $1 $2 $3
+    run_once base_policy false true base_wo_predict $1 $2 $3
     sleep 10
 
-    run_once base_policy true true base_w_predict $client_num $1 $2 $3
+    run_once base_policy true true base_w_predict $1 $2 $3
     sleep 10
 
-    run_once multi_objective_policy false true moo_wo_predict $client_num $1 $2 $3
+    run_once multi_objective_policy false true moo_wo_predict $1 $2 $3
     sleep 10
 
-    run_once multi_objective_policy true true moo_w_predict $client_num $1 $2 $3
+    run_once multi_objective_policy true true moo_w_predict $1 $2 $3
     sleep 10
 
-    run_once multi_objective_policy true false wo_cancel $client_num $1 $2 $3
+    run_once multi_objective_policy true false wo_cancel $1 $2 $3
     sleep 10
 
-    run_once multi_objective_policy true false normal $client_num $1 $2 $3
+    run_once multi_objective_policy true false normal $1 $2 $3
     sleep 10
 }
 
