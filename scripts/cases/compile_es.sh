677e66acc5bb175ca3159f2636a3814bf49dfa78
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-04-07 14:54:36 +0800

    reconstruct for easy compile

--- scripts/cases/compile_es.sh
+++ scripts/cases/compile_es.sh
@@ -8,6 +8,17 @@ if [ -d "$AUTOCANCEL_HOME/elasticsearch/build/distribution/local/elasticsearch-8
     sudo chown -R $(id -u):$(id -u) $AUTOCANCEL_HOME/elasticsearch/build/distribution/local/elasticsearch-8.9.0-SNAPSHOT
 fi
 
+pushd $AUTOCANCEL_HOME/autocancel_java_code
+./gradlew build
+popd
+
+pushd $AUTOCANCEL_HOME/scripts/baseline/psandbox/toy_sandbox
+./gradlew build
+popd
+
+cp $AUTOCANCEL_HOME/autocancel_java_code/lib/build/libs/autocancel-0.0.1.jar $AUTOCANCEL_HOME/elasticsearch/localRepo
+cp $AUTOCANCEL_HOME/scripts/baseline/psandbox/toy_sandbox/lib/build/libs/toysandbox-0.0.1.jar $AUTOCANCEL_HOME/elasticsearch/localRepo
+
 pushd $AUTOCANCEL_HOME/elasticsearch
-./gradlew localDistro
+./gradlew --write-verification-metadata sha256 localDistro
 popd
