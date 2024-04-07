4fc6bf5a50ed8927e12f064db0d5a148792bb0d3
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-04-07 20:10:31 +0800

    update solr compile scripts

--- scripts/cases/compile_solr.sh
+++ scripts/cases/compile_solr.sh
@@ -8,6 +8,17 @@ if [ -d "$AUTOCANCEL_HOME/solr/solr/packaging/build/solr-9.0.0-SNAPSHOT" ]; then
     sudo chown -R $(id -u):$(id -u) $AUTOCANCEL_HOME/solr/solr/packaging/build/solr-9.0.0-SNAPSHOT
 fi
 
+pushd $AUTOCANCEL_HOME/autocancel_java_code
+./gradlew build
+popd
+
+pushd $AUTOCANCEL_HOME/scripts/baseline/psandbox/toy_sandbox
+./gradlew build
+popd
+
+cp $AUTOCANCEL_HOME/autocancel_java_code/lib/build/libs/autocancel-0.0.1.jar $AUTOCANCEL_HOME/solr/localRepo
+cp $AUTOCANCEL_HOME/scripts/baseline/psandbox/toy_sandbox/lib/build/libs/toysandbox-0.0.1.jar $AUTOCANCEL_HOME/solr/localRepo
+
 pushd $AUTOCANCEL_HOME/solr
 ./gradlew assemble
 popd
