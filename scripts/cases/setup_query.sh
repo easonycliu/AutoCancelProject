5b25a80d793a4549f30170e38d37c89295483465
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-05-01 00:05:56 +0800

    tune boolean query size

--- scripts/cases/setup_query.sh
+++ scripts/cases/setup_query.sh
@@ -5,11 +5,11 @@ set -e
 export AUTOCANCEL_HOME=$(git rev-parse --show-toplevel)
 
 if [ ! -f "$AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp/query/boolean_search_1.json" ]; then
-    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/performance_issues/complex_boolean_operations.py 1000 boolean_search_1.json
+    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/performance_issues/complex_boolean_operations.py 1100 boolean_search_1.json
 fi
 
 if [ ! -f "$AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp/query/boolean_search_2.json" ]; then
-    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/performance_issues/complex_boolean_operations.py 2500 boolean_search_2.json
+    docker run --rm --net=host -v $AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp:/root -w /root easonliu12138/es_py_env:v1.1 /root/performance_issues/complex_boolean_operations.py 3500 boolean_search_2.json
 fi
 
 if [ ! -f "$AUTOCANCEL_HOME/autocancel_exp/elasticsearch_exp/query/bulk_large_document.json" ]; then
