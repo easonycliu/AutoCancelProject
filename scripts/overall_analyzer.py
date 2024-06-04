ca2f778ee6dfdf8043cbfeecb493aa62e928425a
liuyc <liuyicheng1515@sjtu.edu.cn>
2024-06-04 23:57:44 +0800

    calc average metric

--- scripts/overall_analyzer.py
+++ scripts/overall_analyzer.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 import numpy as np
 
-from cases_analyzer import analyze_case
+from cases_analyzer import analyze_case, show_case_result
 
 cases_names = ["c1", "c3", "c4", "c5", "c6", "c7"]
 microbenchmark_names = ["abnormal_sensitivity", "rally_benchmark", "solr_bench"]
@@ -14,8 +14,12 @@ baseline_names = ["parties", "psp", "psandbox"]
 if __name__ == "__main__":
 	exp_name = sys.argv[1]
 	exp_type = None
+	analyze_func = None
+	show_result_func = None
 	if exp_name in cases_names:
 		exp_type = "cases"
+		analyze_func = analyze_case
+		show_result_func = show_case_result
 	elif exp_name in microbenchmark_names:
 		exp_type = "microbenchmark"
 	elif exp_name in baseline_names:
@@ -27,6 +31,7 @@ if __name__ == "__main__":
 
 	file_names_df = pd.read_csv(os.path.join(os.getcwd(), "results", exp_type, "{}.csv".format(exp_name)), header=None)
 	file_names_list = list(np.array(file_names_df.values.tolist()).squeeze())
+	avg_throughput_dict, avg_latency_dict, p99_latency_dict, cancel_time_dict, recover_time_dict = analyze_func(file_names_list)
 
-	for file_name in file_names_list:
-		print(analyze_case([file_name]))
+	show_result_func(avg_throughput_dict, avg_latency_dict, p99_latency_dict, cancel_time_dict, recover_time_dict)
+
