16e74bb85924837b2801dd0789703fcc0fe2c240
Yicheng Liu <88122596+easonycliu@users.noreply.github.com>
2025-08-19 12:29:40 -0700

    Update download_data.sh

--- scripts/cases/download_data.sh
+++ scripts/cases/download_data.sh
@@ -4,15 +4,13 @@ set -e
 
 export AUTOCANCEL_HOME=$(git rev-parse --show-toplevel)
 
-gh auth login -p https --with-token <<< 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
-
 if [ ! -d "$AUTOCANCEL_HOME/scripts/data/elasticsearch" ]; then
     mkdir $AUTOCANCEL_HOME/scripts/data/elasticsearch
     pushd $AUTOCANCEL_HOME/scripts/data/elasticsearch
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-es-data -p 'xaa'
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-es-data -p 'xab'
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-es-data -p 'xac'
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-es-data -p 'xad'
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-es-data/xaa
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-es-data/xab
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-es-data/xac
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-es-data/xad
 
     cat xa* > elasticsearch_doc.tar.gz
     tar -xzf elasticsearch_doc.tar.gz
@@ -23,7 +21,7 @@ fi
 if [ ! -d "$AUTOCANCEL_HOME/scripts/data/solr" ]; then
     mkdir $AUTOCANCEL_HOME/scripts/data/solr
     pushd $AUTOCANCEL_HOME/scripts/data/solr
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-solr-data -p 'solr_exp.tar.gz'
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-solr-data/solr_exp.tar.gz
 
     tar -xzf solr_exp.tar.gz
     mv single_node_v9.0/* .
@@ -33,12 +31,12 @@ fi
 if [ ! -d "$AUTOCANCEL_HOME/scripts/data/solr_bench_home" ]; then
     mkdir $AUTOCANCEL_HOME/scripts/data/solr_bench_home
     pushd $AUTOCANCEL_HOME/scripts/data/solr_bench_home
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-solr-bench-data -p 'xaa'
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-solr-bench-data -p 'xab'
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-solr-bench-data -p 'xac'
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-solr-bench-data -p 'xad'
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-solr-bench-data -p 'xae'
-    gh release download --repo liuyc1515/AutoCancelProject v1.0-solr-bench-data -p 'xaf'
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-solr-bench-data/xaa
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-solr-bench-data/xab
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-solr-bench-data/xac
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-solr-bench-data/xad
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-solr-bench-data/xae
+    wget https://github.com/easonycliu/AutoCancelProject/releases/download/v1.0-solr-bench-data/xaf
 
     cat xa* > solr_bench.tar.gz
     tar -xzf solr_bench.tar.gz
@@ -46,4 +44,3 @@ if [ ! -d "$AUTOCANCEL_HOME/scripts/data/solr_bench_home" ]; then
 	mv stress-facets-local-autocancel.json stress-facets-local-autocancel-base.json
     popd
 fi
-
